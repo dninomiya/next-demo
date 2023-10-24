50b10aae5ef5e00239c91720b1274d4cb7040b97
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2023-10-24 12:52:47 +0900

    feat(clerk): add auth ui

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,4 +1,12 @@
 import Path from '@/app/_components/path';
+import { Button } from '@/components/ui/button';
+import {
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs';
 import React from 'react';
 
 export default function Header() {
@@ -8,6 +16,17 @@ export default function Header() {
         <h1>Next.js Demo</h1>
         <Path />
         <div className="flex-1"></div>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        <SignedOut>
+          <SignInButton afterSignUpUrl="/">
+            <Button variant="outline">ログイン</Button>
+          </SignInButton>
+          <SignUpButton>
+            <Button>アカウント作成</Button>
+          </SignUpButton>
+        </SignedOut>
       </div>
     </header>
   );
--- app/_components/navigation.tsx
+++ app/_components/navigation.tsx
@@ -1,3 +1,5 @@
+import { auth } from '@clerk/nextjs/server';
+import { Globe, Lock, Unlock } from 'lucide-react';
 import Link from 'next/link';
 import React from 'react';
 
@@ -14,6 +16,8 @@ const pages = [
 ];
 
 export default function Navigation() {
+  const { userId } = auth();
+
   return (
     <div className="border text-sm rounded-md flex flex-col divide-y overflow-hidden col-span-1">
       {pages.map((page) => (
@@ -22,6 +26,21 @@ export default function Navigation() {
           href={page.path}
           className="px-4 py-3 flex items-center gap-4 hover:bg-slate-50"
         >
+          {page.private ? (
+            <span>
+              {userId ? (
+                <Unlock size={16} className="text-muted-foreground" />
+              ) : (
+                <Lock size={16} className="text-muted-foreground" />
+              )}
+              <span className="sr-only">プライベート</span>
+            </span>
+          ) : (
+            <span>
+              <Globe size={16} className="text-muted-foreground" />
+              <span className="sr-only">公開</span>
+            </span>
+          )}
           <span>{page.title}</span>
         </Link>
       ))}

e79b39d49eb87d4d75f58a4efccfd1b3deb3ac25
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2023-10-24 20:54:35 +0900

    up

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -8,7 +8,9 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <div className="bg-white mx-auto max-w-4xl rounded-xl shadow-lg">
         <Header authDisabled />
         <div className="grid grid-cols-3 px-6 gap-6 py-6">
-          <div className="cols-span-1">{/* <Navigation /> */}</div>
+          <div className="cols-span-1">
+            <Navigation />
+          </div>
           <main className="col-span-2">{children}</main>
         </div>
       </div>

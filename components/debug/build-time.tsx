c620e9817288a4f74c96b7b3b0ee98cb291c03a1
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2023-10-24 20:44:36 +0900

    up

--- components/debug/build-time.tsx
+++ components/debug/build-time.tsx
@@ -1,9 +1,10 @@
-import { format } from 'date-fns';
+import { formatInTimeZone } from 'date-fns-tz';
 
 export default function BuildTime() {
   return (
     <p className="text-xs italic text-muted-foreground">
-      ビルド日時: {format(new Date(), 'yyyy年MM月dd日 HH:mm:ss')}
+      ビルド日時:
+      {formatInTimeZone(new Date(), 'Asia/Tokyo', 'yyyy年MM月dd日 HH:mm:ss')}
     </p>
   );
 }

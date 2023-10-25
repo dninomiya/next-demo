ae4d08b80351c660690f4dd32f675344c4e20569
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2023-10-25 18:50:49 +0900

    fix

--- app/static/a/page.tsx
+++ app/static/a/page.tsx
@@ -2,7 +2,7 @@ import { saveBuildTimestamp } from '@/actions/revalidate';
 import PageContent from '@/app/_components/page-content';
 
 export default function Page() {
-  // saveBuildTimestamp('a');
+  saveBuildTimestamp('a');
 
   return <PageContent isStatic title="A" />;
 }

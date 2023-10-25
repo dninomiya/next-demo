54ff34211b2bb25853924e8ab35654ed2208d48c
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2023-10-25 18:38:31 +0900

    up

--- app/demo/page.tsx
+++
@@ -1,8 +0,0 @@
-import { format } from 'date-fns';
-import React from 'react';
-
-export default function Page() {
-  const date = new Date();
-
-  return <div>{format(date, 'yyyy/MM/dd HH:mm:ss')}</div>;
-}

b37af0a9351becb17d2f26a58f3ed0d43a8e9cd6
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2023-10-26 16:50:01 +0900

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,4 @@
-import { ArrowRoot } from '@/components/arrow';
-import Browser from '@/components/browser';
-import { CacheStatusProvider } from '@/components/cache-status-provider';
-import FlowMap from '@/components/flow-map';
-import { buildTimestamp } from '@/lib/config';
 import type { Metadata } from 'next';
-import { Suspense } from 'react';
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -20,7 +14,8 @@ export default async function RootLayout({
   return (
     <html lang="ja">
       <body>
-        <CacheStatusProvider timestamp={buildTimestamp}>
+        {children}
+        {/* <CacheStatusProvider timestamp={buildTimestamp}>
           <ArrowRoot>
             <FlowMap />
             <hr className="border-dashed" />
@@ -28,7 +23,7 @@ export default async function RootLayout({
               <Browser>{children}</Browser>
             </div>
           </ArrowRoot>
-        </CacheStatusProvider>
+        </CacheStatusProvider> */}
       </body>
     </html>
   );

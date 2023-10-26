186c69ba09184a6c52f45f2fd65b299bf07f801d
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2023-10-26 16:59:42 +0900

    tes

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic';
+
+import PageContent from '@/components/page-content';
+
+export default function Page() {
+  return <PageContent isStatic title="Top" />;
+}
